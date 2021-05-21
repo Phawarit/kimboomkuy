1b20a8f71ea6b8e87c74c6f7f4ea7dd89aa581c6
Phawarit <boomphawarit@kkumail.com>
2021-05-21 15:30:04 +0700

    add register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BannerComponent } from './banner/banner.component';
+import { RegisterComponent } from './register/register.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    BannerComponent
+    BannerComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    RouterModule.forRoot([{
+      path: '', component: TopBarComponent,
+
+    },
+
+    { path: 'registerpage', component: RegisterComponent },
+    
+    ])
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
