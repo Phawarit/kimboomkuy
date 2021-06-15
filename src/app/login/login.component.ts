40c409cdb380d7e5b049bd98f0f9c2f19be74e06
Phawarit <boomphawarit@kkumail.com>
2021-06-15 14:26:50 +0700

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -10,33 +11,80 @@ import {Router} from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  form!: FormGroup;
-
- 
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient,
-    private router: Router
-
-
-
-  ) { }
+  submit: boolean = false;
+  checkemail: boolean =false;
+  form = new FormGroup({
+  
+    
+    // email: new FormControl('', [
+    //   Validators.required,
+    //   Validators.email
+    // ]),
+    // password: new FormControl('', [
+    //   Validators.required,
+    //   Validators.minLength(6)
+    // ])
+    
+   });
+  constructor( 
+    private formBuilder:FormBuilder,
+    private http:HttpClient,
+    private router:Router) { }
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      email: '',
-      password: ''
+      email:['',[Validators.required,
+        Validators.email]],
+      password:['',[Validators.required,
+        Validators.minLength(6)]]
     });
+
+    console.log(this.form);
+    
+  }
+  onSubmit(){
+    console.log(this.form);
+    
+    alert(JSON.stringify(this.form.value));
   }
 
-  submit(): void {
-    this.http.post('http://api.arumirite.codes/login', this.form.getRawValue(), {
-      withCredentials: true
-    }).subscribe(() => this.router.navigate(['/home']));
+
+  Submit(): void {
+    console.log(this.form);
+   
+    this.submit = true
+    if (this.form.valid == true) {
+      this.http.post('https://api.arumirite.codes/login', this.form.getRawValue())
+      .subscribe(() => this.router.navigate(['/home'])); 
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'เข้าสู่ระบบเรียบร้อยจ้า',
+        showConfirmButton: false,
+        timer: 1700
+      })
+   
+    
+    
+    
+    
+    }
+    else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'กรุณากรอกข้อมูลให้ถูกต้อง',
+        
+      })
+    }
+    
+
+
+
+
+   
   }
 
 
-  }
 
-
+}
